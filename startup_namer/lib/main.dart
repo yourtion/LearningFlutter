91199330df95583b3be286bef8870aadc7db8134
YourtionGuo <yourtion@gmail.com>
2018-05-13 15:38:43 +0800

    startup_namer: 有状态的部件（Stateful widget）

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -6,9 +6,6 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    final wordPair = new WordPair.random();
-
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
@@ -16,9 +13,23 @@ class MyApp extends StatelessWidget {
           title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(wordPair.asCamelCase),
+          child: new RandomWords(),
         ),
       ),
     );
   }
 }
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
+  }
+}
+
